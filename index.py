8ac9807e406f40b5826896358a48bfc78aa63bde
kshitijbhatt <kbhatt14king@gmail.com>
2019-07-16 16:46:47 +0530

    total calculation

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 app.debug = True
@@ -9,9 +9,16 @@ def index():
     return "Hello World! Python works but auto reload doesnt"
 
 
-@app.route('/flight/domestic<name>')
-def hello_name(name):
-    return 'Hello %s!' % name
+@app.route('/values', methods=['POST', 'GET'])
+def values():
+    if request.method == "POST":
+        international_flight = float(request.form['internationalFlight']) * 0.10
+        domestic_flight = float(request.form['domesticFlight']) * 0.12
+        train = float(request.form['train']) * 0.01
+        bus = float(request.form['bus']) * 0.02
+        car = float(request.form['car']) * 0.19
+        total = international_flight + domestic_flight + train + bus + car
+    return jsonify(total)
 
 
 if __name__ == "__main__":
