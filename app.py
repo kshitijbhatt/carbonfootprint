dae73d96685d99d055686b9976982d4565dfc388
fre2mansur <fre2mansur@gmail.com>
2019-07-16 14:46:46 +0530

    co2 calculation added

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, escape, request
-
+from flask import Flask, escape, request,json
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def index():
+    co2 = request.args.get("co2")
+    atree_offset = 22
+    atree_life = 20
+    offset = int(co2) / atree_offset / atree_life
+    atree_survive = 80
+    total_trees = offset * 100 / atree_survive
+    atree_cost = 300
+    total_cost = total_trees * atree_cost
+
+    data = {
+        "co2": co2,
+        "offset": offset,
+        "num_trees": total_trees,
+        "tree_cost": atree_cost,
+        "total_cost": total_cost
+    }
+    data = json.dumps(data)
+    return str(data)
 
 
 if __name__ == "__main__":
