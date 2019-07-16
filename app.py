1e3c2e7a55c4816752702d2a5b5aa3ebd1a571f3
fre2mansur <fre2mansur@gmail.com>
2019-07-16 14:54:26 +0530

    rounded all decimal value

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    co2 = request.args.get("co2")
+    co2 = int(request.args.get("co2"))
     atree_offset = 22
     atree_life = 20
-    offset = int(co2) / atree_offset / atree_life
+    offset = int(round(co2 / atree_offset / atree_life))
     atree_survive = 80
-    total_trees = offset * 100 / atree_survive
+    total_trees = int(round(offset * 100 / atree_survive))
     atree_cost = 300
-    total_cost = total_trees * atree_cost
+    total_cost = int(round(total_trees * atree_cost))
 
     data = {
         "co2": co2,
