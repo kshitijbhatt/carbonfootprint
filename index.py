26124ef594a38a642d61d8fbd855e8e4f3544a77
aura-network <aura.auroville@gmail.com>
2020-05-28 14:58:05 +0530

    corrections in api calculations

--- index.py
+++ index.py
@@ -18,20 +18,21 @@ def results():
         bus = float(request.form['bus']) * 0.02
         car = float(request.form['car']) * 0.19
         total = international_flight + domestic_flight + train + bus + car
-        co2 = float(round(total))
+        co2 = float(total)
         absorption_rate = 22
         life_of_tree = 20
         survival_rate = 80
         cost_per_tree = 300
-        total_offset = int(round(co2 / absorption_rate / life_of_tree))
+        total_offset = (co2 / absorption_rate) / life_of_tree
         total_trees = int(round(total_offset * 100 / survival_rate))
-        total_cost = int(round(total_trees * cost_per_tree))
+        total_cost = int(total_trees * cost_per_tree)
         data = [{
-            "totalEmission": co2,
+            "totalEmission": total_offset,
             "noOfTrees": total_trees,
             "totalCost": total_cost
         }]
-        return render_template('results.html', data=data)
+        # return render_template('results.html', data=data)
+        return jsonify(data)
 
 
 if __name__ == "__main__":
